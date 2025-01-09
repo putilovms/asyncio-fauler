4f9399e90da2e9ea7dcaa83918d6af2872c1ef43
Mickle <putilovms@gmail.com>
2025-01-09 21:02:36 +0400

    modified task2.py

--- task/task2.py
+++ task/task2.py
@@ -13,9 +13,10 @@ async def main():
     tasks = [asyncio.create_task(print_with_delay(num)) for num in range(10)]
     _, broken = await asyncio.wait(tasks, timeout=15)
     if broken:
-        print(f'Есть прерванные задачи - {len(broken)} шт')
-        for task in broken:
-            task.cancel()
+        [task.cancel() for task in broken]
+        print(f'Прервано задач - {len(broken)} шт')
+
+            
     print(f'Заняло {time.time() - start:.1f} с')
 
 
