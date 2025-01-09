a9fa4f6f8765bf44610395adb60ebd3b26e3c364
Mickle <putilovms@gmail.com>
2025-01-09 20:56:18 +0400

    add task

---
+++ task/task2.py
@@ -0,0 +1,20 @@
+import asyncio
+import time
+
+
+async def print_with_delay(num):
+    await asyncio.sleep(1)
+    print(f'Coroutine {num} is done')
+
+
+async def main():
+    start = time.time()
+
+    tasks = [asyncio.create_task(print_with_delay(num)) for num in range(10)]
+    _, broken = await asyncio.wait(tasks, timeout=15)
+    if broken:
+        print(f'Есть прерванные задачи - {len(broken)} шт')
+    print(f'Заняло {time.time() - start:.1f} с')
+
+
+asyncio.run(main())
